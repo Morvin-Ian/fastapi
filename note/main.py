1408ca40167acf4a1d52c1e4c5c857da44df6b4b
Morvin-Ian <morvinian@gmail.com>
2024-10-25 21:05:19 +0300

    edits on models

--- note/main.py
+++ note/main.py
@@ -5,8 +5,10 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class Note(BaseModel):
-    pass
+    title: str
+    body: str
+    completed
 
 @app.post('/api/v1/create')
 async def create_note(note: Note):
-    return {"message": "User created successfully"}
+    return {"message": "Note created successfully"}
