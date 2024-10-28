94e601f8885dcd4dc0ec135996767f9e5b55075b
Morvin-Ian <morvinian@gmail.com>
2024-10-29 02:45:53 +0300

    fix: jwt auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import  OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
+from typing import Annotated
 
 from ..database import get_db
 from .. import schemas, models
@@ -18,12 +19,13 @@ router = APIRouter(
 )
 
 @router.post('/token', response_model=schemas.Token)
-def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login(request: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email==request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
     if not Hash.verify(password=request.password, hashed_password=user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+    
     access_token_expires = timedelta(minutes=int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')))
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
